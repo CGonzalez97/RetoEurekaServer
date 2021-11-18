43ba362106a7b00852c40fd7d4b151aa4ad192a2
CGonzalez97 <cgl76490@gmail.com>
2021-11-18 12:19:13 +0100

    Subida EurekaServer

---
+++ src/main/java/com/ntt_data_bootcamp/microservicios/BootcampNttDataRetoEurekaServerApplication.java
@@ -0,0 +1,15 @@
+package com.ntt_data_bootcamp.microservicios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class BootcampNttDataRetoEurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootcampNttDataRetoEurekaServerApplication.class, args);
+	}
+
+}
